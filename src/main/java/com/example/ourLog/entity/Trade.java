62e9aafe70b659fa886d60a0457c67cd5f203434
Ethualo <lkh0926ym@naver.com>
2025-04-01 16:28:14 +0900

    Update Trade.java

--- src/main/java/com/example/ourLog/entity/Trade.java
+++ src/main/java/com/example/ourLog/entity/Trade.java
@@ -15,7 +15,7 @@ import lombok.*;
 public class Trade extends BaseEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id1;
+  private Long id;
 
   @OneToOne
   private Long picNo;
