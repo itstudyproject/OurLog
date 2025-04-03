091a566139827acf168432a1b817e278a64d8e9e
12345-happy <skeara@naver.com>
2025-04-03 13:32:49 +0900

    250403

---
+++ src/main/java/com/example/ourLog/controller/BookmarkController.java
@@ -0,0 +1,14 @@
+package com.example.ourLog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/bookmark")
+@RequiredArgsConstructor
+
+public class BookmarkController {
+  
+}
