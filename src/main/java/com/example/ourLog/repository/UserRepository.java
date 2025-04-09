a036fd6c3857f616e8392bcc9d4555ada530405c
Ethualo <lkh0926ym@naver.com>
2025-04-09 17:02:37 +0900

    0409 edit

--- src/main/java/com/example/ourLog/repository/UserRepository.java
+++ src/main/java/com/example/ourLog/repository/UserRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User,String> {
@@ -18,4 +19,11 @@ public interface UserRepository extends JpaRepository<User,String> {
   @Query("select u from User u where u.email = :email")
   Optional<User> findByEmail(@Param("email") String email);
 
+  @EntityGraph(attributePaths = {"roleSet"}, type = EntityGraph.EntityGraphType.LOAD)
+  @Query("select u from User u where u.userId = :userId")
+  Optional<User> findByUserId(@Param("userId") Long userId);
+
+  @Modifying
+  @Query("delete from User u where u.userId = :userId")
+  void deleteByUserId(@Param("userId") Long userId);
 }
