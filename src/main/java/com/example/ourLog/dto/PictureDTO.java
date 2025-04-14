2caa7496dcd7f390972a8c8bc001195c313e88ff
XII1071 <garmen1071@gmail.com>
2025-04-14 13:23:16 +0900

    0414

--- src/main/java/com/example/ourLog/dto/PictureDTO.java
+++ src/main/java/com/example/ourLog/dto/PictureDTO.java
@@ -5,9 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
 @Data
 @Builder
 @AllArgsConstructor
@@ -16,20 +13,4 @@ public class PictureDTO {
   private String uuid;
   private String picName;
   private String path;
-  public String getPictureURL() {
-    try {
-      return URLEncoder.encode(path + "/" + uuid + "_" + picName, "UTF-8");
-    } catch (UnsupportedEncodingException e) {
-      e.printStackTrace();
-    }
-    return "";
-  }
-  public String getThumbnailURL() {
-    try {
-      return URLEncoder.encode(path + "/s_" + uuid + "_" + picName, "UTF-8");
-    } catch (UnsupportedEncodingException e) {
-      e.printStackTrace();
-    }
-    return "";
-  }
 }
