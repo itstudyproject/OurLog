d038e6dbb15d6aebaf3167d8a67ef633d45052f6
Ethualo <lkh0926ym@naver.com>
2025-04-15 10:11:29 +0900

    searchRepImpl edit

--- src/main/java/com/example/ourLog/repository/search/SearchRepositoryImpl.java
+++ src/main/java/com/example/ourLog/repository/search/SearchRepositoryImpl.java
@@ -1,5 +1,9 @@
 package com.example.ourLog.repository.search;
 
+import com.example.ourLog.entity.Post;
+import com.example.ourLog.entity.QPicture;
+import com.example.ourLog.entity.QPost;
+import com.example.ourLog.entity.QReply;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Order;
@@ -21,27 +25,27 @@ import java.util.stream.Collectors;
 public class SearchRepositoryImpl extends QuerydslRepositorySupport
     implements SearchRepository {
   public SearchRepositoryImpl() {
-    super(Movie.class);
+    super(Post.class);
   }
 
   @Override
   public Page<Object[]> searchPage(String type, String keyword, Pageable pageable) {
     //1) q도메인을 확보
-    QPicture movie = QMovie.movie;
-    QMovieImage movieImage = QMovieImage.movieImage;
-    QReview review = QReview.review;
+    QPost post = QPost.post;
+    QPicture picture = QPicture.picture;
+    QReply reply = QReply.reply;
 
     //2) q도메인을 조인
-    JPQLQuery<Movie> jpqlQuery = from(movie);
-    jpqlQuery.leftJoin(movieImage).on(movieImage.movie.eq(movie));
-    jpqlQuery.leftJoin(review).on(review.movie.eq(movie));
+    JPQLQuery<Post> jpqlQuery = from(post);
+    jpqlQuery.leftJoin(picture).on(picture.postId.eq(post));
+    jpqlQuery.leftJoin(reply).on(reply.postId.eq(post));
 
     //3) Tuple생성 : 조인을 한 결과의 데이터를 tuple로 생성
-    JPQLQuery<Tuple> tuple = jpqlQuery.select(movie, movieImage, review.grade.avg().coalesce(0.0),review.count());
+    JPQLQuery<Tuple> tuple = jpqlQuery.select(post, picture, reply.count());
 
     //4) 조건절 생성
     BooleanBuilder booleanBuilder = new BooleanBuilder();
-    BooleanExpression expression = movie.mno.gt(0L);
+    BooleanExpression expression = post.postId.gt(0L);
     booleanBuilder.and(expression);
 
     //5) 검색조건 파악
@@ -51,11 +55,11 @@ public class SearchRepositoryImpl extends QuerydslRepositorySupport
       for (String t : typeArr) {
         switch (t) {
           case "t":
-            conditionBuilder.or(movie.title.contains(keyword)); break;
+            conditionBuilder.or(post.title.contains(keyword)); break;
           case "w":
-            conditionBuilder.or(review.member.email.contains(keyword)); break;
+            conditionBuilder.or(reply.nickname.nickname.contains(keyword)); break;
           case "c":
-            conditionBuilder.or(review.text.contains(keyword)); break;
+            conditionBuilder.or(reply.text.contains(keyword)); break;
         }
       }
       booleanBuilder.and(conditionBuilder);
@@ -68,12 +72,12 @@ public class SearchRepositoryImpl extends QuerydslRepositorySupport
     sort.stream().forEach(order -> {
       Order direction = order.isAscending() ? Order.ASC : Order.DESC;
       String prop = order.getProperty();
-      PathBuilder orderByExpression = new PathBuilder(Movie.class, "movie");
+      PathBuilder orderByExpression = new PathBuilder(Post.class, "post");
       tuple.orderBy(new OrderSpecifier<Comparable>(direction, orderByExpression.get(prop)));
     });
 
     //8) 데이터를 출력하기 위한 그룹을 생성
-    tuple.groupBy(movie);
+    tuple.groupBy(post);
 
     //9) 원하는 데이터를 들고오기 위해서 시작위치즉 offset을 지정
     tuple.offset(pageable.getOffset());
