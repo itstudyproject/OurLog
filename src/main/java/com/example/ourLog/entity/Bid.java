139bcc9ae43436114f4376cfb012969986931c3a
XII1071 <garmen1071@gmail.com>
2025-04-25 13:39:01 +0900

    0425

--- src/main/java/com/example/ourLog/entity/Bid.java
+++ src/main/java/com/example/ourLog/entity/Bid.java
@@ -1,4 +1,33 @@
 package com.example.ourLog.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Bid {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long bidId;
+
+  // 입찰 금액
+  private Long amount;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "trade_id")
+  private Trade trade;
+
+  // 입찰자
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "bidder_id")
+  private User user;
+
+  private LocalDateTime bidTime;
 }
