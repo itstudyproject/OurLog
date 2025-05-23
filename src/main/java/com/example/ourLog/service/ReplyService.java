bdcfef75368f77b61d1e3e6bd5ec147a1b2f86b5
XII <garmen1071@gmail.com>
2025-05-23 16:04:49 +0900

    0523

--- src/main/java/com/example/ourLog/service/ReplyService.java
+++ src/main/java/com/example/ourLog/service/ReplyService.java
@@ -10,42 +10,50 @@ import com.example.ourLog.entity.User;
 import java.util.List;
 
 public interface ReplyService {
-  Long register(Long postId, ReplyDTO replyDTO);
 
-  List<ReplyDTO> getList(Long postId);
+    Long register(Long postId, ReplyDTO replyDTO);
 
-  void modify(ReplyDTO replyDTO);
+    List<ReplyDTO> getList(Long postId);
 
-  void remove(Long replyId);
+    void modify(ReplyDTO replyDTO);
 
-  default Reply dtoToEntity(ReplyDTO replyDTO) {
-    Reply reply = Reply.builder()
-        .replyId(replyDTO.getReplyId())
-        .post(Post.builder()
-                .postId(replyDTO.getPostDTO().getPostId())
-                .build())
-        .user(User.builder()
-                .userId(replyDTO.getUserDTO().getUserId())
-                .build())
-        .content(replyDTO.getContent())
-        .build();
-    return reply;
-  }
+    void remove(Long replyId);
 
+    default Reply dtoToEntity(ReplyDTO replyDTO) {
+        Reply reply = Reply.builder()
+                .replyId(replyDTO.getReplyId())
+                .post(Post.builder()
+                        .postId(replyDTO.getPostDTO().getPostId())
+                        .build())
+                .user(User.builder()
+                        .userId(replyDTO.getUserDTO().getUserId())
+                        .build())
+                .content(replyDTO.getContent())
+                .build();
+        return reply;
+    }
 
-  default ReplyDTO entityToDto(Reply reply) {
-    ReplyDTO replyDTO = ReplyDTO.builder()
-        .replyId(reply.getReplyId())
-        .postDTO(PostDTO.builder()
-                .postId(reply.getPost().getPostId())
-                .build())
-        .userDTO(UserDTO.builder()
-                .userId(reply.getUser().getUserId())
-                .build())
-        .content(reply.getContent())
-        .regDate(reply.getRegDate())
-        .modDate(reply.getModDate())
-        .build();
-    return replyDTO;
-  }
+    default ReplyDTO entityToDto(Reply reply) {
+        User replyUser = reply.getUser();
+
+        UserDTO userDTO = null;
+        if (replyUser != null) {
+            userDTO = UserDTO.builder()
+                    .userId(replyUser.getUserId())
+                    .nickname(replyUser.getNickname())
+                    .build();
+        }
+
+        ReplyDTO replyDTO = ReplyDTO.builder()
+                .replyId(reply.getReplyId())
+                .postDTO(PostDTO.builder()
+                        .postId(reply.getPost().getPostId())
+                        .build())
+                .userDTO(userDTO)
+                .content(reply.getContent())
+                .regDate(reply.getRegDate())
+                .modDate(reply.getModDate())
+                .build();
+        return replyDTO;
+    }
 }
