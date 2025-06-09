50264455fda6572bfe97ea64848de1f5cfec904a
Ethualo <lkh0926ym@naver.com>
2025-06-09 11:08:25 +0900

    CORS 수정

--- src/main/java/com/example/ourLog/config/SecurityConfig.java
+++ src/main/java/com/example/ourLog/config/SecurityConfig.java
@@ -185,7 +185,7 @@ public class SecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
       CorsConfiguration configuration = new CorsConfiguration();
       configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173"));
-      configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+      configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
       configuration.setAllowCredentials(true);
       configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Request-ID"));
       configuration.setExposedHeaders(Arrays.asList("X-Request-ID"));
